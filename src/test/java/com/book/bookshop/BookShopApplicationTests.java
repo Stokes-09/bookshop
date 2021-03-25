4a6fa58b4527ebd7bf393a270475996a0dbd7546
Stokes-09 <614978141@qq.com>
2021-03-25 20:51:47 +0800

    第一天

---
+++ src/test/java/com/book/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
